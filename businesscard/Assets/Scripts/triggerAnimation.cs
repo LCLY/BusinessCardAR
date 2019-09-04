f612cd238b1a93e9ee228161a754daa564f7670b
lchoo <lchoo1996@gmail.com>
2019-09-03 23:21:43 -0700

    refactor: added anim buttons

---
+++ businesscard/Assets/Scripts/triggerAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerAnimation : MonoBehaviour
+{
+    public GameObject charObj;
+    Animator charAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+       charAnim = charObj.GetComponent<Animator>();
+        
+    }
+
+    public void WaveAnim()
+    {
+        charAnim.SetTrigger("Wave");
+    }
+
+    public void PickUpAnim()
+    {
+        charAnim.SetTrigger("Pickup");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
