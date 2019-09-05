773f54f2c72f079aefe7fe262960de55807c2bd7
lchoo <lchoo1996@gmail.com>
2019-09-05 15:18:23 -0700

    refactor: create a new scss to change bootstrap container

--- web/src/Components/Content.js
+++ web/src/Components/Content.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Row, Col } from "react-bootstrap";
 const Content = () => {
-    return <div>YES</div>;
+    return (
+        <div>
+            <div className="project__desc">
+                <div className="project__title">
+                    <h1>
+                        <b>Project Description:</b>
+                    </h1>
+                </div>
+                <div className="project__desc--content">
+                    An augmented reality application built using Unity and
+                    Vuforia, allows user to scan an image target and it will
+                    generate certain 3D models
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Content;
