e5e94869f9119e2c4bea6bfb7a37ebbd4aa29589
lchoo <lchoo1996@gmail.com>
2019-09-04 17:03:56 -0700

    Updates

---
+++ precache-manifest.8d2cc9665f6027865d87a97292057dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b78e997c8008b14fc6ae7c4085f7d1",
+    "url": "/BusinessCardAR/index.html"
+  },
+  {
+    "revision": "3ee0b2a780d87bac9c4b",
+    "url": "/BusinessCardAR/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b4197565ddff3d74f874",
+    "url": "/BusinessCardAR/static/js/2.765fc547.chunk.js"
+  },
+  {
+    "revision": "3ee0b2a780d87bac9c4b",
+    "url": "/BusinessCardAR/static/js/main.07361bc5.chunk.js"
+  },
+  {
+    "revision": "da45f1112c8912d273bb",
+    "url": "/BusinessCardAR/static/js/runtime~main.0131e16e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BusinessCardAR/static/media/logo.5d5d9eef.svg"
+  }
+]);
