4bc50e2b2a2e0296347c680c36609f7433de57bc
lchoo <lchoo1996@gmail.com>
2019-09-06 03:58:59 -0700

    Updates

---
+++ precache-manifest.27b78666d42ce5740312498c12761d4f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b412b38ee983b8d8da0b413ebf5be0",
+    "url": "/BusinessCardAR/index.html"
+  },
+  {
+    "revision": "22def6f4f3377f3d5bc8",
+    "url": "/BusinessCardAR/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fdcb7066b13ba5fdfdc0",
+    "url": "/BusinessCardAR/static/css/main.1f95a011.chunk.css"
+  },
+  {
+    "revision": "22def6f4f3377f3d5bc8",
+    "url": "/BusinessCardAR/static/js/2.77d2fc89.chunk.js"
+  },
+  {
+    "revision": "fdcb7066b13ba5fdfdc0",
+    "url": "/BusinessCardAR/static/js/main.a302fdf3.chunk.js"
+  },
+  {
+    "revision": "da45f1112c8912d273bb",
+    "url": "/BusinessCardAR/static/js/runtime~main.0131e16e.js"
+  },
+  {
+    "revision": "14686b8607f138e4d5f3690a52250716",
+    "url": "/BusinessCardAR/static/media/CardAR.14686b86.apk"
+  },
+  {
+    "revision": "8f0f1c6488365bdddd240f07af86e871",
+    "url": "/BusinessCardAR/static/media/back.8f0f1c64.jpg"
+  },
+  {
+    "revision": "e8c6e793f1eacc4cf77e55cee287f977",
+    "url": "/BusinessCardAR/static/media/backcard.e8c6e793.jpg"
+  },
+  {
+    "revision": "a227a7a338d95d4abff245bd2ba2c6e1",
+    "url": "/BusinessCardAR/static/media/card.a227a7a3.jpg"
+  },
+  {
+    "revision": "793c855ed0eb68e9d4ca9532fd285144",
+    "url": "/BusinessCardAR/static/media/cardInstruction.793c855e.jpg"
+  },
+  {
+    "revision": "114087cb8f046b2f22dbb11a6cf50d9f",
+    "url": "/BusinessCardAR/static/media/cardar-cut.114087cb.mp4"
+  },
+  {
+    "revision": "7cf312418a38da3bab861910a2dba50e",
+    "url": "/BusinessCardAR/static/media/front.7cf31241.jpg"
+  },
+  {
+    "revision": "3a955bd83929b53ad2beb9491dd730a3",
+    "url": "/BusinessCardAR/static/media/frontcard.3a955bd8.jpg"
+  },
+  {
+    "revision": "1c47b3f52507d258c16dc131bd57e2cd",
+    "url": "/BusinessCardAR/static/media/print.1c47b3f5.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BusinessCardAR/precache-manifest.916b3aee5d277f132e8f242b51a6d149.js"
+  "/BusinessCardAR/precache-manifest.27b78666d42ce5740312498c12761d4f.js"
 );
 
 self.addEventListener('message', (event) => {
