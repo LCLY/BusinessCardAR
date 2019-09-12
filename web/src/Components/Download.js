2d7165caf6addc0ca47804ede0af663f944fdca7
lchoo <lchoo1996@gmail.com>
2019-09-12 03:38:58 -0700

    refactor: removed icons from download

--- web/src/Components/Download.js
+++ web/src/Components/Download.js
@@ -11,63 +11,9 @@ const Download = () => {
                     &nbsp; Download Apk
                 </Button>
             </a>
-            <div style={{ paddingTop: "0.5rem" }}>
-                <a
-                    href="https://github.com/LCLY"
-                    target="_blank"
-                    style={{ color: "inherit" }}
-                >
-                    <i
-                        className="fab fa-github footer__icon"
-                        style={{
-                            padding: "0 0.5rem 0 0.5rem",
-                            fontSize: "2rem",
-                        }}
-                    />
-                </a>
-                <a
-                    href="https://linkedin.com/in/lcly9294/"
-                    target="_blank"
-                    style={{ color: "inherit" }}
-                >
-                    <i
-                        className="fab fa-linkedin footer__icon"
-                        style={{
-                            padding: "0 0.5rem 0 0.5rem",
-                            fontSize: "2rem",
-                        }}
-                    />
-                </a>
-                <a
-                    href="mailto:lchoo9294@gmail.com"
-                    target="_blank"
-                    style={{ color: "inherit" }}
-                >
-                    <i
-                        className="fas fa-envelope footer__icon"
-                        style={{
-                            padding: "0 0.5rem 0 0.5rem",
-                            fontSize: "2rem",
-                        }}
-                    />
-                </a>
-                <a
-                    href="https://lcly.github.io/personalwebsite/"
-                    target="_blank"
-                    style={{ color: "inherit" }}
-                >
-                    <i
-                        className="fas fa-globe footer__icon"
-                        style={{
-                            padding: "0 0.5rem 0 0.5rem",
-                            fontSize: "2rem",
-                        }}
-                    />
-                </a>
-            </div>
             <div className="download__footer" style={{ paddingTop: "0.5rem" }}>
-                Only Android for now, might require disabling Play Protect
-                during installation.
+                Only on Android for now, <br />
+                might require disabling Play Protect during installation.
             </div>
         </div>
     );
